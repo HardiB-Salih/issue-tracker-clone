bccf8601fb5f0f139625ede76dbf3bff4068fd83
HardiB-Salih <hardib.salih@gmail.com>
2024-08-05 23:31:56 +0300

    Set up Radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@radix-ui/themes";
+
 interface IssuesPageProps {
   // Define your props here
 }
 
 export default function IssuesPage({}: IssuesPageProps) {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
