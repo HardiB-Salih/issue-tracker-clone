6ee3e554e126cc3590d635df3fbd541dbc1be629
HardiB-Salih <hardib.salih@gmail.com>
2024-08-06 01:11:43 +0300

    implement clint side validation.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import kyInstance from "@/lib/ky";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useState } from "react";
@@ -9,16 +9,23 @@ import { useForm, Controller } from "react-hook-form";
 
 import "easymde/dist/easymde.min.css";
 import { InfoIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/lib/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 export default function NewIssuePage() {
   const [error, setError] = useState("");
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   const onCreate = async (values: IssueForm) => {
     try {
@@ -29,8 +36,6 @@ export default function NewIssuePage() {
       setError("An unexpected error occured");
     }
   };
-
-  //   className={isDarkMode ? "dark-mode" : "light-mode"}
 
   return (
     <div className="max-w-xl">
@@ -44,6 +49,11 @@ export default function NewIssuePage() {
       )}
       <form onSubmit={handleSubmit(onCreate)} className="space-y-3">
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Text as="p" color="red">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -51,6 +61,12 @@ export default function NewIssuePage() {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text as="p" color="red">
+            {errors.description.message}
+          </Text>
+        )}
+
         <Button type="submit">Submit new issue</Button>
       </form>
     </div>
