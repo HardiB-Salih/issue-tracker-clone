20decb6a23dd65c51c2bb807687ad656b983520d
HardiB-Salih <hardib.salih@gmail.com>
2024-08-06 10:32:57 +0300

    detail page created

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingNewIssue() {
+  return <div>Loading...</div>;
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/prisma-client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
 import IssueActions from "./issue-actions";
+import Link from "next/link";
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
@@ -27,7 +28,7 @@ export default async function IssuesPage() {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="flex w-full md:hidden">
                   <IssueStatusBadge className="ml-auto" status={issue.status} />
                 </div>
